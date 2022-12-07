c325063b0148a94c08ca0c310408029b8017350b
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-07 15:05:31 +0500

    created FirstGoal component

---
+++ src/components/FirstGoal.js
@@ -0,0 +1,10 @@
+const FirstGoal = () => {
+    return(
+        <li>
+            <h3>First Goal</h3>
+            <p>The first goal is to study react basics</p>
+        </li>
+    );
+}
+
+export default FirstGoal
