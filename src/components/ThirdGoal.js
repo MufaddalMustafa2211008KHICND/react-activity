0e0e1ab172a37f0a69b71b8f8f00efe34a6a9890
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-07 15:07:54 +0500

    created ThirdGoal Component

---
+++ src/components/ThirdGoal.js
@@ -0,0 +1,10 @@
+const ThirdGoal = () => {
+    return(
+        <li>
+            <h3>Third Goal</h3>
+            <p>The third goal is to practice react</p>
+        </li>
+    );
+}
+
+export default ThirdGoal
