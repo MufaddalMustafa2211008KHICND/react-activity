11d92e82ec7a821d92977a297abd799f22b631fa
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-07 15:09:55 +0500

    created GoalsList component

---
+++ src/components/GoalsList.js
@@ -0,0 +1,15 @@
+import FirstGoal from './FirstGoal';
+import SecondGoal from './SecondGoal';
+import ThirdGoal from './ThirdGoal';
+
+const GoalsList = () => {
+    return(
+        <ul>
+            <FirstGoal/>
+            <SecondGoal/>
+            <ThirdGoal/>
+        </ul>
+    );
+}
+
+export default GoalsList
