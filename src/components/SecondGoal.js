c42afafb685ed85867678d439b877f69837aea5d
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-07 15:06:44 +0500

    created SecondGoal Component

---
+++ src/components/SecondGoal.js
@@ -0,0 +1,10 @@
+const SecondGoal = () => {
+    return(
+        <li>
+            <h3>Second Goal</h3>
+            <p>The second goal is to create components</p>
+        </li>
+    );
+}
+
+export default SecondGoal
